23feb4113728ecf93ba32601843784309a33fba0
lclror <l95818416@hotmail.com>
2016-01-21 17:26:57 +0800

    Revert "del"
    This reverts commit 83114ebf6dd1f830d7145104b8a4c5041c34e2d9.

---
+++ node_start.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./'))
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+/*var P_new01=require('./p_news/node_P_new01')
+P_new01.routerall(app)*/
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app.listen(3000,function(){console.log('running ')})
---
+++ r_build.js
@@ -0,0 +1,6 @@
+({
+	app:'app',
+	baseUrl:'P_news',
+	paths:{},
+	module:{}	
+})
