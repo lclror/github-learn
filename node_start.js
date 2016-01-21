5a223a34745fba7fdb4bfff19517f5b777ebcc99
lclror <l95818416@hotmail.com>
2016-01-21 17:05:40 +0800

    node_start  to change01

--- node_start.js
+++ node_start.js
@@ -4,13 +4,9 @@ app.use(express.static('./'))
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-/*var node_news=require('./node_news')
-node_news.routerall(app)*/
 
-/*var G_paging=require('./G_paging/node_G_paging')
-G_paging.ajax(app)*/
 
-var P_new01=require('./p_news/node_P_new01')
-P_new01.routerall(app)
+/*var P_new01=require('./p_news/node_P_new01')
+P_new01.routerall(app)*/
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.listen(3000,function(){console.log('running ')})
