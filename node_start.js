19e7eedc2ed907157d94af4d21672f3e7290fb58
lclror <l95818416@hotmail.com>
2016-01-21 17:29:08 +0800

    test

--- node_start.js
+++ node_start.js
@@ -8,5 +8,7 @@ app.use(express.static('./'))
 
 /*var P_new01=require('./p_news/node_P_new01')
 P_new01.routerall(app)*/
+var test=require('./test/test')
+test.routerall(app)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.listen(3000,function(){console.log('running ')})
